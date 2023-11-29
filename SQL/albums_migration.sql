ab0f724440653d4e41f84d735c5f002a9b282461
Anthony Gantt <anthony@pcsatease.com>
2023-11-29 15:01:50 -0600

    sql:tables exercise completed.

---
+++ SQL/albums_migration.sql
@@ -0,0 +1,14 @@
+CREATE Table albums (
+      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+          artist STRING(50) NOT NULL,
+          name STRING(100) NOT NULL,
+          release_data INT,
+          sales FLOAT(6,2) not null,
+          genre STRING
+);
+
+
+
+
+
+
