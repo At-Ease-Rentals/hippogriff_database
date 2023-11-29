91a421dfe05911c7419dad8d6f716b96bc571fb9
Anthony Gantt <anthony@pcsatease.com>
2023-11-29 15:39:32 -0600

    sql:completed basic textbook examples.

--- SQL/in_class_examples.sql
+++ SQL/in_class_examples.sql
@@ -15,3 +15,36 @@ WHERE id = 1;
 SELECT author_last_name, content FROM quotes;
 
 SELECT * FROM quotes;
+
+SELECT * FROM quotes WHERE author_last_name = 'Adams';
+
+
+INSERT INTO quotes (author_first_name, author_last_name, content)
+VALUES ('Douglas', 'Adams', 'Time is an illusion. Lunchtime doubly so.'),('Douglas', 'Adams', 'I love deadlines. I love the whooshing noise they make as they go by.');
+
+SELECT  * FROM quotes WHERE author_last_name = 'Adams';
+
+SELECT * FROM quotes WHERE id = 5;
+
+
+SELECT * FROM quotes WHERE  id = 3;
+
+SELECT 'I am output!' AS 'Info';
+
+UPDATE quotes
+SET author_first_name = 'Samuel', author_last_name = 'Clemens'
+WHERE  id = 4;
+
+SHOW tables
+
+SELECT * FROM quotes WHERE id = 4;
+
+SELECT * FROM quotes;
+
+DELETE FROM quotes WHERE id = 3;
+
+SELECT  * FROM quotes;
+
+TRUNCATE quotes;
+
+SELECT * FROM quotes;
